587c8d463af0965438c284748b9b187a97dae903
Robert Glonek <rglonek@aerospike.com>
2024-07-17 11:03:45 -0700

    add dockerfile generator script

---
+++ gen-dockerfiles.sh
@@ -0,0 +1,30 @@
+sources=(
+    ubuntu:24.04
+    ubuntu:22.04
+    ubuntu:20.04
+    debian:12
+    debian:11
+    debian:10
+    debian:9
+    rockylinux:9
+    rockylinux:8
+    quay.io/centos/centos:stream9
+)
+
+for i in ${sources[@]}; do
+targetName="${i/quay.io\/centos\/centos:stream/centos}"
+targetName="${targetName/:/}"
+targetName="Dockerfile-${targetName/./}"
+cat <<EOF > $targetName
+FROM golang:1.21 AS build
+WORKDIR /src
+COPY . /src/
+RUN cp forkpreload/fork_amd64.so systemd/fork.so
+RUN cp forkpreload/fakefork_amd64.so systemd/fakefork.so
+RUN env CGO_ENABLED=0 go build -trimpath -ldflags="-s -w" -o /usr/sbin/init-docker-systemd .
+
+FROM $i
+COPY --from=build /usr/sbin/init-docker-systemd /usr/sbin/init-docker-systemd
+ENTRYPOINT ["/usr/sbin/init-docker-systemd"]
+EOF
+done
